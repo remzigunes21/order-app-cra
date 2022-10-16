fce69ceb9268df4626a2495c4172b8ca9d0fd149
remzi.gunes <remzi.gunes@odeontechnology.com>
2022-10-16 22:27:50 +0300

    modified review

--- src/containers/HomeContainer/ReviewSection.jsx
+++ src/containers/HomeContainer/ReviewSection.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Swiper, SwiperSlide} from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Scrollbar, A11y, Autoplay } from "swiper";
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+
 function ReviewSection() {
   return (
     <section className="review" id="review">
@@ -15,15 +16,14 @@ function ReviewSection() {
       <Swiper
         spaceBetween={25}
         slidesPerView={3}
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        modules={[Pagination, Scrollbar, A11y, Autoplay]}
         navigation
+        autoplay={{ delay: 4000 }}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-        className="swiper review-slider"
-      >
-            
+        onSlideChange={() => console.log("slide change")}
+        className="swiper review-slider">
         <SwiperSlide>
           <div className="swiper-slide box">
             <i className="fas fa-quote-left"></i>
@@ -95,76 +95,6 @@ function ReviewSection() {
           </div>
         </SwiperSlide>
       </Swiper>
-
-      {/* <div className="swiper review-slider">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide box">
-            <i class="fas fa-quote-left"></i>
-            <i class="fas fa-quote-right"></i>
-            <img src={process.env.PUBLIC_URL + "image/pic-1.png"} alt="" />
-            <div class="stars">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aliquid eveniet qui, similique aut sit.</p>
-            <h3>john deo</h3>
-            <span>satisfied client</span>
-          </div>
-
-          <div class="swiper-slide box">
-            <i class="fas fa-quote-left"></i>
-            <i class="fas fa-quote-right"></i>
-            <img src={process.env.PUBLIC_URL + "image/pic-2.png"} alt="" />
-            <div class="stars">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aliquid eveniet qui, similique aut sit.</p>
-            <h3>john deo</h3>
-            <span>satisfied client</span>
-          </div>
-
-          <div class="swiper-slide box">
-            <i class="fas fa-quote-left"></i>
-            <i class="fas fa-quote-right"></i>
-            <img src={process.env.PUBLIC_URL + "image/pic-3.png"} alt="" />
-            <div class="stars">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aliquid eveniet qui, similique aut sit.</p>
-            <h3>john deo</h3>
-            <span>satisfied client</span>
-          </div>
-
-          <div class="swiper-slide box">
-            <i class="fas fa-quote-left"></i>
-            <i class="fas fa-quote-right"></i>
-            <img src={process.env.PUBLIC_URL + "image/pic-4.png"} alt="" />
-            <div class="stars">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aliquid eveniet qui, similique aut sit.</p>
-            <h3>john deo</h3>
-            <span>satisfied client</span>
-          </div>
-        </div>
-
-        <div class="swiper-pagination"></div>
-      </div> */}
     </section>
   );
 }
